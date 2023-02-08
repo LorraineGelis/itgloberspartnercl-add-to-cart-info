2334b42f682115e9be107ea0e4a67be6b30261ef
Lorraine <lgelisdiaz@outlook.com>
2023-02-07 19:01:30 -0500

    feat:css styles are made for the add to cart component implemented on desktop tablet and mobile

--- react/components/ButtonGroup.tsx
+++ react/components/ButtonGroup.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-/* import { generateBlockClass } from '@vtex/css-handles'
+import { generateBlockClass } from '@vtex/css-handles'
 
-import style from './style.css' */
+import styles from './styles.css'
 
-const ButtonGroup = () => {
+const ButtonGroup = ({ blockClass }: { blockClass: string }) => {
+
+    const container__buttons = generateBlockClass(styles.container__buttons, blockClass)
+    const button__checkout = generateBlockClass(styles.button__checkout, blockClass)
+    const button__cart = generateBlockClass(styles.button__cart, blockClass)
     return (
         <>
-            <div>
-                <a href="/">Ckeck out</a>
+            <div className={container__buttons}>
+                <a className={button__checkout} href="/">Continúa la compra</a>
+                <a className={button__cart} href="https://storecompragamer--itgloberspartnercl.myvtex.com/checkout/#/cart">Ver carrito</a>
             </div>
-
-            <button>Continúa comprando</button>
-            <a href="/">Ver carrito</a>
         </>
     )
 }
